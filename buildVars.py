fed2c254065eaf62ef94fece30e9ff819474f73d
David Parduhn <xkill85@gmx.net>
2019-12-09 10:30:58 +0100

    Aktualisierung auf NVDA 2019.3 Beta 1

--- buildVars.py
+++ buildVars.py
@@ -20,13 +20,14 @@ addon_info = {
 	"addon_description" : _("""Diese Erweiterung verbessert die Zugaenglichkeit von Kaspersky Internet Security
 2013 bis 2019."""),
 	# version
-	"addon_version" : "0.14",
+	"addon_version" : "0.15",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
+	"lastTestedNVDAVersion": "2019.3.0"
 }
 
 

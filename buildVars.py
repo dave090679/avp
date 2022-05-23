c5c47771bfe35f14e7121c2aaf4a5c8c70f83b4f
David Parduhn <xkill85@gmx.net>
2022-05-23 16:26:00 +0200

    - update 2 nvda 2022.1

--- buildVars.py
+++ buildVars.py
@@ -18,16 +18,16 @@ addon_info = {
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Diese Erweiterung verbessert die Zugaenglichkeit von Kaspersky Internet Security
-2013 bis 2019."""),
+2013 bis 2021."""),
 	# version
-	"addon_version" : "0.20",
+	"addon_version" : "0.21",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
-	"lastTestedNVDAVersion": "2021.1"
+	"lastTestedNVDAVersion": "2022.1"
 }
 
 

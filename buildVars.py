a68e6ca39e0a7dacbc0fb67e40e238d0f88f4f8f
David Parduhn <xkill85@gmx.net>
2023-06-20 21:14:51 +0200

    0.22: - temporarily commented out a custom menuitem class (fixes an issue while expanding a submenu in context menus) might crash in some other menus:).

--- buildVars.py
+++ buildVars.py
@@ -20,14 +20,14 @@ addon_info = {
 	"addon_description" : _("""Diese Erweiterung verbessert die Zugaenglichkeit von Kaspersky Internet Security
 2013 bis 2021."""),
 	# version
-	"addon_version" : "0.21",
+	"addon_version" : "0.22",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
-	"lastTestedNVDAVersion": "2022.1"
+	"lastTestedNVDAVersion": "2023.1"
 }
 
 
